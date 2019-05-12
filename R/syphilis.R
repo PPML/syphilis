71306b590eceb132dc11b540653695a6dd74af59
Christian Testa <testa101@gmail.com>
2019-05-12 15:58:17 -0400

    Rename the project to syphilis

--- R/syphilis.R
+++ R/syphilis.R
@@ -1,4 +1,4 @@
-#' syphLAMA A model of syphilis fit to data from Louisiana and Massachusetts
+#' A model of syphilis fit to data from Louisiana and Massachusetts
 #' @useDynLib syphLAMA, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 "_PACKAGE"
