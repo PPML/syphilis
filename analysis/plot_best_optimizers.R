ff37286d7c94e06f7984a3217513cb35e9580550
Christian Testa <testa101@gmail.com>
2019-06-10 10:29:11 -0400

    Fix package name to syphilis in analysis scripts

--- analysis/plot_best_optimizers.R
+++ analysis/plot_best_optimizers.R
@@ -3,13 +3,13 @@ library(here)
 
 state <<- 'LA'
 load.start()
-trace.burn.thin <- trace.burn <- trace <- readRDS(system.file('optims', 'la_top5_pars.rds', package='syphLAMA'))
+trace.burn.thin <- trace.burn <- trace <- readRDS(system.file('optims', 'la_top5_pars.rds', package='syphilis'))
 post.sample <- model.fits(trace, use_trace_without_sampling=T)
 plot.posteriors(post.sample, output_dir=here('inst/optims/'))
 
 
 state <<- 'MA'
 load.start()
-trace.burn.thin <- trace.burn <- trace <- readRDS(system.file('optims', 'ma_top5_pars.rds', package='syphLAMA'))
+trace.burn.thin <- trace.burn <- trace <- readRDS(system.file('optims', 'ma_top5_pars.rds', package='syphilis'))
 post.sample <- model.fits(trace, use_trace_without_sampling=T)
 plot.posteriors(post.sample, output_dir=here('inst/optims/'))
