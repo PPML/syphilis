3fcf23d0e76618829a96cc280ccd7b8393e670d5
Christian Testa <testa101@gmail.com>
2019-08-02 14:10:18 -0400

    Update MCMC processing/rendering code

--- analysis/mcmc/plot_mcmc_results.R
+++ analysis/mcmc/plot_mcmc_results.R
@@ -1,11 +1,11 @@
 library(here)
 devtools::load_all()
 
-output_directory <- here("inst/mcmc/5-2-19/")
+output_directory <- here("inst/mcmc/8-1-19/")
 for (state in c("LA", "MA")) {
   load.start()
   trace <- trace.burn.thin <- trace.burn <- readRDS(
-    file.path(output_directory, paste0("trace_burn_and_thinned_", state, ".rds")))
+    file.path(output_directory, paste0("trace_", tolower(state), "_burned_and_thinned.rds")))
   
   post.sample <- model.fits(trace, use_trace_without_sampling = T)
   pred <- as.data.frame(post.sample$outputs)
