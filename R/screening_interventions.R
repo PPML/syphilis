bf3a19376290f01cb9168a1b1e885eae625e3ae1
Christian Testa <testa101@gmail.com>
2019-08-09 11:11:04 -0400

    Add functions to plot screening interventions

--- R/screening_interventions.R
+++ R/screening_interventions.R
@@ -13,14 +13,16 @@ modify_simulation_environment_for_an_intervention <- function(e, intervention) {
 	switch(intervention, 
 									annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 1),
 						twice_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 2),
-			 twelve_times_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 8),
+			 twelve_times_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 12),
+   twenty_four_times_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 24),
+   sixteen_times_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 16),
 							msm_annual = adjust_screening_for_intervention(e, pop_indices = c(m4,m5), new_level = 1),
 				msm_twice_annual = adjust_screening_for_intervention(e, pop_indices = c(m4,m5), new_level = 2),
 			 msm_hivpos_annual = adjust_screening_for_intervention(e, pop_indices = m5, new_level = 1),
- msm_hivpos_twice_annual = adjust_screening_for_intervention(e, pop_indices = m5, new_level = 2),
+   msm_hivpos_twice_annual = adjust_screening_for_intervention(e, pop_indices = m5, new_level = 2),
 			 msm_hivneg_annual = adjust_screening_for_intervention(e, pop_indices = m4, new_level = 1),
- msm_hivneg_twice_annual = adjust_screening_for_intervention(e, pop_indices = m4, new_level = 2),
- msm_annual_hr_msm_quarterly = {
+   msm_hivneg_twice_annual = adjust_screening_for_intervention(e, pop_indices = m4, new_level = 2),
+   msm_annual_hr_msm_quarterly = {
 		adjust_screening_for_intervention(e, pop_indices = c(m4,m5), new_level = 1)
 		adjust_screening_for_intervention(
 			e, 
@@ -28,29 +30,40 @@ modify_simulation_environment_for_an_intervention <- function(e, intervention) {
 				setdiff(c(m4, m5), setdiff(1:40, pop$index[pop$k == 'high'])),
 			new_level = 4)
 	 },
- msm_8x_annually = adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 8),
- hr_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
- hr_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
- msm_annual_hr_and_prior_diagnosis_quarterly = {
-   adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
-   adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4)
-    e$params$screen_repeat[106:nrow(e$params$screen_repeat), ] <- 4
-	 },
- msm_annual_all_hr_annual = {
-   adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
-   adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1)
-	 },
- msm_annual_all_hr_half_annual = {
-   adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
-   adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = .5)
-	 },
- prior_diagnosis_quarterly = {
-    e$params$screen_repeat[106:nrow(e$params$screen_repeat), ] <- 
-			4
-	 },
- high_activity_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
- high_activity_twice_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 2),
- high_activity_quarterly = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4)
+   msm_8x_annually = adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 8),
+   hr_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
+   hr_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
+   msm_annual_hr_and_prior_diagnosis_quarterly = {
+     adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
+     adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4)
+      e$params$screen_repeat[106:nrow(e$params$screen_repeat), ] <- 4
+     },
+
+   msm_hr_and_prior_diagnosis_quarterly = {
+     adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 4)
+     adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4)
+      e$params$screen_repeat[106:nrow(e$params$screen_repeat), ] <- 4
+     },
+   msm_annual_all_hr_annual = {
+     adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
+     adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1)
+     },
+   msm_annual_all_hr_half_annual = {
+     adjust_screening_for_intervention(e, pop_indices = c(m4, m5), new_level = 1)
+     adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = .5)
+     },
+   prior_diagnosis_quarterly = {
+      e$params$screen_repeat[106:nrow(e$params$screen_repeat), ] <- 
+        4
+     },
+   high_activity_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 1),
+   high_activity_twice_annual = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 2),
+   high_activity_quarterly = adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4),
+   annual_hr_quarterly = { 
+     adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 1)
+     adjust_screening_for_intervention(e, pop_indices = high_activity, new_level = 4)
+   },
+   half_annual = adjust_screening_for_intervention(e, pop_indices = 1:32, new_level = 0.5)
 	)
 
   return(invisible(NULL))
@@ -137,6 +150,53 @@ extractInterventionStatistics <- function(sol) {
 }
 
 
+extract_outcomes_by_sex <- function(sol) { 
+	intervention_start <- min(intervention_years)
+	intervention_period <- seq((intervention_start-1)*52-1, model.end*52)
+
+  decumulate <- function(idxs) { 
+    return(
+      rowSums(sol[intervention_period, idxs]) - 
+      rowSums(sol[intervention_period - 52, idxs])
+    )} 
+
+  retrieve <- function(idxs) { 
+    rowSums(sol[intervention_period, idxs])
+  }
+
+  data.frame(
+    year = intervention_period/52 + model_to_gregorian_difference,
+    # popsizes
+    popsize_all = retrieve(allpop.index),
+    popsize_females = retrieve(females),
+    popsize_msw = retrieve(msw),
+    popsize_msm = retrieve(msm),
+
+    # diagnoses 
+    diagnosed_all = decumulate(c(d1.index, d2.index, d3.index)), # diagnosed early syphilis
+    diagnosed_females = decumulate(diagnosed_females),
+    diagnosed_msw = decumulate(diagnosed_msw),
+    diagnosed_msm = decumulate(diagnosed_msm),
+
+    # incidence
+    incidence_all = decumulate(c(inc.index, incr.index)),
+    incidence_females = decumulate(incidence_females),
+    incidence_msw = decumulate(incidence_msw),
+    incidence_msm = decumulate(incidence_msm),
+
+    # prevalence
+    prevalence_all = retrieve(early_infected_index),
+    prevalence_females = retrieve(prevalence_early_females),
+    prevalence_msw = retrieve(prevalence_early_msw),
+    prevalence_msm = retrieve(prevalence_early_msm),
+
+    # tests
+    tests_all = decumulate(tested.index)
+  )
+
+}
+
+
 extract_diagnoses_by_sex <- function(sol) { 
 	intervention_start <- min(intervention_years)
 	intervention_period <- seq((intervention_start-1)*52-1, model.end*52)
@@ -217,7 +277,18 @@ extract_prevalence_by_sex <- function(sol) {
 #' df <- simulate_interventions(theta)
 simulate_interventions <- function(
   theta, 
-  interventions_list = c('msm_annual_hr_msm_quarterly', 'prior_diagnosis_quarterly', 'high_activity_quarterly', 'msm_annual_hr_and_prior_diagnosis_quarterly'),
+  interventions_list = c('msm_annual_hr_msm_quarterly',
+  'prior_diagnosis_quarterly', 'high_activity_quarterly',
+  'msm_annual_hr_and_prior_diagnosis_quarterly',
+  'annual',
+  'half_annual',
+  'msm_hr_and_prior_diagnosis_quarterly',
+  'msm_annual_all_hr_half_annual',
+  'msm_annual_all_hr_annual',
+  'twelve_times_annual',
+  'sixteen_times_annual',
+  'twenty_four_times_annual'
+  ),
   info_extractor
 ) {
 	intervention_outcomes <- list()
@@ -232,6 +303,56 @@ simulate_interventions <- function(
 	do.call(rbind.data.frame, intervention_outcomes)
 }
 
+
+
+simulate_outcomes_by_sex_by_state <- function(state) { 
+  
+  state <<- state
+  # load state model
+  load.start()
+
+  # get state parametrization
+  theta <- get(paste0('theta_', tolower(state)))
+
+  # simulate interventions, record diagnoses by sex
+  df <- simulate_interventions(theta, info_extractor = extract_outcomes_by_sex)
+
+  df %<>% 
+  # reshape data into rates per 100k
+  dplyr::mutate(
+    # prevalence 
+    prevalence_all = prevalence_all / popsize_all * 1e5,
+    prevalence_females = prevalence_females / popsize_females * 1e5,
+    prevalence_msw = prevalence_msw / popsize_msw * 1e5,
+    prevalence_msm = prevalence_msm / popsize_msm * 1e5,
+
+    # incidence 
+    incidence_all = incidence_all / popsize_all * 1e5,
+    incidence_females = incidence_females / popsize_females * 1e5,
+    incidence_msw = incidence_msw / popsize_msw * 1e5,
+    incidence_msm = incidence_msm / popsize_msm * 1e5,
+
+    # diagnoses
+    diagnosed_all = diagnosed_all / popsize_all * 1e5,
+    diagnosed_females = diagnosed_females / popsize_females * 1e5,
+    diagnosed_msw = diagnosed_msw / popsize_msw * 1e5,
+    diagnosed_msm = diagnosed_msm / popsize_msm * 1e5) %>% 
+
+  # remove population sizes
+  select(
+    -c(popsize_females, 
+       popsize_msw, 
+       popsize_msm,
+       tests_all)) %>% 
+
+  # reshape the columns into rows with column-variables
+  reshape2::melt(id.vars = c('intervention', 'year'), value.name = 'rate') %>% 
+
+  # separate the variable column into variable and sex
+  tidyr::separate(col = variable, into = c('variable', 'sex'), extra='drop')
+
+  return(df)
+}
 
 
 simulate_diagnoses_by_sex_by_state <- function(state) { 
@@ -311,6 +432,70 @@ simulate_prevalence_by_sex_by_state <- function(state) {
   return(df)
 }
 
+simulate_outcomes_by_sex_both_states <- function() { 
+  rbind.data.frame(
+    cbind.data.frame(state = 'Louisiana', simulate_outcomes_by_sex_by_state('LA')),
+    cbind.data.frame(state = 'Massachusetts', simulate_outcomes_by_sex_by_state('MA')))
+}
+
+#' @example 
+#' df <- simulate_outcomes_by_sex_both_states()
+#' plot_list <- plot_outcomes_by_sex(df, save=F)
+plot_outcomes_by_sex <- function(df) { 
+  plot_list <- list()
+
+  df %<>% filter(year <= 2027, year >= 2017)
+
+  produce_plot <- function(state, sex, outcome, include_title = T) { 
+    state1 <- enquo(state)
+    sex1 <- enquo(sex)
+    outcome1 <- enquo(outcome)
+
+    sex_lookup <- c(females = 'F', msw = 'MSW', msm = 'MSM', all = 'All')
+
+    ggplot( data = filter(df, state == !! state1, sex == !! sex1, variable == !! outcome1), 
+            mapping = aes(x = year, y = rate, color = intervention, linetype = intervention)) + 
+      geom_line(size = 1) + 
+      ylab('') + 
+      xlab('') + 
+      expand_limits(y=0) + 
+      # scale_color_manual(values = c('red', 'forestgreen', 'skyblue', 'orange', 'coral', 'purple', 'pink', 'grey', 'grey1', 'grey2', 'grey3', 'grey4')) + 
+      scale_x_continuous(breaks = seq(2017, 2027, by=2)) + 
+      theme_bw() + 
+      if (include_title) { 
+        ggtitle(paste0(tools::toTitleCase(outcome), ' Rate per 100,000'), subtitle = paste0(sex_lookup[[sex]], " - ", state)) 
+      } else { NULL } 
+  }
+}
+
+
+
+plot_state_outcomes_by_sex <- function(state, sex, outcome, include_title = T) { 
+
+  if (! exists('df') || 
+  colnames(df) != c("state", "intervention", "year", "variable", "sex", "rate")) { 
+    stop("df should be produced using simulate_outcomes_by_sex_both_states()")
+  }
+
+  state1 <- enquo(state)
+  sex1 <- enquo(sex)
+  outcome1 <- enquo(outcome)
+
+  sex_lookup <- c(females = 'F', msw = 'MSW', msm = 'MSM', all = 'All')
+
+  ggplot( data = filter(df, state == !! state1, sex == !! sex1, variable == !! outcome1), 
+          mapping = aes(x = year, y = rate, color = intervention, linetype = intervention)) + 
+    geom_line(size = 1) + 
+    ylab('') + 
+    xlab('') + 
+    expand_limits(y=0) + 
+    # scale_color_manual(values = c('red', 'forestgreen', 'skyblue', 'orange', 'coral', 'purple', 'pink', 'grey', 'grey1', 'grey2', 'grey3', 'grey4')) + 
+    scale_x_continuous(breaks = seq(2017, 2027, by=2)) + 
+    theme_bw() + 
+    if (include_title) { 
+      ggtitle(paste0(tools::toTitleCase(outcome), ' Rate per 100,000'), subtitle = paste0(sex_lookup[[sex]], " - ", state)) 
+    } else { NULL } 
+}
 
 #' @import tidyverse
 simulate_and_plot_diagnoses_by_sex_both_states <- function() { 
