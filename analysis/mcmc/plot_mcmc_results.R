12dad1c0875350b36c4e3d947c0914eda0d22237
Christian Testa <testa101@gmail.com>
2019-09-20 13:50:00 -0400

    Make plot_mcmc_results work from pre-computed post.sample

--- analysis/mcmc/plot_mcmc_results.R
+++ analysis/mcmc/plot_mcmc_results.R
@@ -10,8 +10,9 @@ for (state in c("LA", "MA")) {
   trace <- trace.burn.thin <- trace.burn <- readRDS(
     file.path(output_directory, paste0("trace_", tolower(state), "_burned_and_thinned.rds")))
   
-  post.sample <- model.fits(trace, use_trace_without_sampling = T)
+  # post.sample <- model.fits(trace, use_trace_without_sampling = T)
+  post.sample <- readRDS(paste0(output_directory, state, "_posterior_sample.rds"))
   pred <- as.data.frame(post.sample$outputs)
-  saveRDS(post.sample, paste0(output_directory, state, "_posterior_sample.rds"))
+  # saveRDS(post.sample, paste0(output_directory, state, "_posterior_sample.rds"))
   plot.posteriors(output_directory, post.sample = post.sample)
 }
