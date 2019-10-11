34a344e45d701bdd0bc1df816127f31fc77066f1
Christian Testa <testa101@gmail.com>
2019-10-11 13:50:48 -0400

    Remove commented out debugging code

--- analysis/plot_best_optimizers.R
+++ analysis/plot_best_optimizers.R
@@ -6,7 +6,6 @@ optim_path <- here("inst/optims/10-8-19/")
 state <<- 'LA'
 load.start()
 trace.burn.thin <- trace.burn <- trace <- readRDS(paste0(optim_path, 'la_top5_best_pars.rds'))
-# trace <- rbind.data.frame(theta_la, theta_la, theta_la, theta_la, theta_la)
 colnames(trace) <- names(theta_la)
 trace.burn.thin <- trace.burn <- trace
 post.sample <- model.fits(trace, use_trace_without_sampling=T)
@@ -17,7 +16,6 @@ plot.posteriors(post.sample, output_dir=optim_path)
 state <<- 'MA'
 load.start()
 trace.burn.thin <- trace.burn <- trace <- readRDS(paste0(optim_path, 'ma_top5_best_pars.rds'))
-# trace <- rbind.data.frame(theta_ma, theta_ma, theta_ma, theta_ma, theta_ma)
 colnames(trace) <- names(theta_ma)
 trace.burn.thin <- trace.burn <- trace
 post.sample <- model.fits(trace, use_trace_without_sampling=T)
