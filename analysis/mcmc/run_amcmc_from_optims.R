65bd4c77b2a4b89e8008eea1f923233928e0fdb4
Christian Testa <testa101@gmail.com>
2019-10-13 11:02:43 -0400

    Setup MCMC for 10-13

--- analysis/mcmc/run_amcmc_from_optims.R
+++ analysis/mcmc/run_amcmc_from_optims.R
@@ -5,18 +5,19 @@ devtools::load_all()
 
 args <- commandArgs(trailingOnly = T)
 array_id <- args[[1]]
-output_dir <- here("inst/mcmc/9-24-19/")
+output_dir <- here("inst/mcmc/10-13-19/")
+input_dir <- here("inst/optims/10-12-19/")
 
 
 ### Construct dLogPosterior_simultaneous to calibrate to both 
 source(here("analysis/optimize/construct_simultaneous_optimization_function.R"))
 
 
-top_optim_trace_path_la <- paste0(here(paste0("inst/optims/9-23-19/la_top5_best_pars.rds")))
+top_optim_trace_path_la <- paste0(input_dir, "la_top5_best_pars.rds")
 optim_trace_la <- readRDS(top_optim_trace_path_la)
 theta_la <- unlist(optim_trace_la[ (as.numeric(array_id) %% 5)+1, ])
 
-top_optim_trace_path_ma <- paste0(here(paste0("inst/optims/9-23-19/ma_top5_best_pars.rds")))
+top_optim_trace_path_ma <- paste0(input_dir, "/ma_top5_best_pars.rds")
 optim_trace_ma <- readRDS(top_optim_trace_path_ma)
 theta_ma <- unlist(optim_trace_ma[ (as.numeric(array_id) %% 5)+1, ])
 
