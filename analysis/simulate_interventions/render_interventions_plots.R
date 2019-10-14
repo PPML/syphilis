23717469bf24efb5a2acd98b88482d8ffe78cd21
Christian Testa <testa101@gmail.com>
2019-10-14 13:46:01 -0400

    Update intervention figures

---
+++ analysis/simulate_interventions/render_interventions_plots.R
@@ -0,0 +1,16 @@
+
+devtools::load_all()
+
+compute_confidence_intervals_for_trace_interventions()
+
+compute_interventions_change_in_outcomes()
+
+make_incidence_diagnoses_intervention_plots()
+
+# save_outcome_plots_with_confidence_intervals()
+
+# plot_point_estimates_panel()
+
+plot_point_estimates_of_relative_change_panel() # this doesn't seem to be working quite right yet 
+
+plot_violin_boxplot_change_in_intvs_panels() # or this...
